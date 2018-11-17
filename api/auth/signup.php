77aab48f2f87c2b56b5ce1e5a2717233a97dba2e
Huỳnh Hà <huynhsamha@gmail.com>
2018-11-17 07:31:51 +0700

    add crypto password

--- api/auth/signup.php
+++ api/auth/signup.php
@@ -3,16 +3,19 @@
 include_once __DIR__.DIRECTORY_SEPARATOR.'../_post.php';
 
 $email = retrieve_post('email');
-$username = retrieve_post('username');
-$password = retrieve_post('password');
 $first_name = retrieve_post('first_name');
 $last_name = retrieve_post('last_name');
 $tel = retrieve_post('tel');
+$address = retrieve_post('address');
+$password = retrieve_post('password');
+
+$salt = bin2hex(random_bytes(32)); # 32 bytes => 32*8 bits => hex: 32*8/4 (1 hex = 4 bits)
+$password = hash('sha256', $password . $salt);
 
 echo json_encode(array(
-    'username' => $username,
     'email' => $email,
-    'password' => $password
+    'password' => $password,
+    'salt' => $salt
 ));
 
 ?>
